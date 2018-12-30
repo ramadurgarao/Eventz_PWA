d9863d699e173f673763873b282da827eadf65ad
unknown <ramdurga.giet@gmail.com>
2018-12-30 12:32:56 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/index.html 	modified:   src/styles.css 	modified:   src/tsconfig.app.json 	.firebase/ 	.firebaserc 	firebase.json 	ngsw-config.json 	src/app/comments-carousel/ 	src/app/gallery-carousel/ 	src/app/menu/ 	src/assets/bg.jpg 	src/assets/bg.png 	src/assets/birthday.jpg 	src/assets/commercial.png 	src/assets/corporate.jpg src/assets/cparties.jpg 	src/assets/icons/ 	src/assets/personal.jpg 	src/assets/sticky.jpg 	src/assets/sticky.png 	src/assets/wedding.jpg 	src/firebase-messaging-sw.js 	src/manifest.json 	src/sw.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { AppComponentGallaryComponent } from './comments-carousel/comments-carousel.component';
+import { AppImageGallaryComponent } from './gallery-carousel/gallery-carousel.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AppComponentGallaryComponent,
+    AppImageGallaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
